7d3f1340a5a0c25f87e797874e126ae4468d6ea0
numerology <jxzheng@google.com>
2019-08-21 09:50:36 -0700

    add random files

---
+++ playground.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+
+class helperClass:
+  def __init__(self):
+    self.helloworld = 8
+    print( self.helloworld + self.helloworld + self.helloworld + self.helloworld + self.helloworld + self.helloworld)
+    pass
